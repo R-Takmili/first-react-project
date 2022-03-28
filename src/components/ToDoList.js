418bb89297bd0cb25b4e052d39d9e3c1254bae12
unknown <raziye.takmili@yahoo.com>
2022-03-29 00:20:20 +0430

    first commit

---
+++ src/components/ToDoList.js
@@ -0,0 +1,11 @@
+function ToDoList (){
+return(
+    <>     
+    <div className='card'>
+     <h2 style={{textAlign:"center"}}>TITLE</h2>
+     <button className='buttonStyle'>Delete</button> 
+    </div>
+    </>
+    );
+}
+export default ToDoList;
